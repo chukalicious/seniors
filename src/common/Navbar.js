965b88bfb639d938531d8a210b54d9ba1862e68f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-03 02:05:35 -0800

    removed hidden property on the lg screens in the navbar that was preventing the menu button from displaying

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="navbar bg-neutral text-neutral-content">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <label tabIndex={0} className="btn btn-ghost">
             What's Inside
           </label>
           <ul
