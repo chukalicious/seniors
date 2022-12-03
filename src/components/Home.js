621c5c99eeb9259fda24bf33adf1865b08f15e26
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-02 21:44:30 -0800

    added hero component to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
 const Home = () => {
-  return;
+  return (
+    <div
+      className="hero min-h-[75vh]"
+      style={{
+        backgroundImage: `url("https://live.staticflickr.com/65535/52539291794_382460c3b1_o.jpg")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-sm mt-40">
+          <h1 className="mb-5 text-5xl font-bold">
+            Your one-stop Resource Guide
+          </h1>
+          <button className="btn btn-primary">See What's Inside</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
