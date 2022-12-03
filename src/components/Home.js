980156ab0494b5c03b175de1e2479a8d5001638f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-03 01:37:24 -0800

    removed opacity, placed the h1 lower over the image and removed the text size

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,12 @@ const Home = () => {
         backgroundImage: `url("https://live.staticflickr.com/65535/52539291794_382460c3b1_o.jpg")`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-sm mt-40">
-          <h1 className="mb-5 text-5xl font-bold">
+        <div className="max-w-sm mt-40 md:w-md">
+          <h1 className="mt-44 text-4xl font-bold uppercase">
             Your one-stop Resource Guide
           </h1>
-          <button className="btn btn-primary">See What's Inside</button>
         </div>
       </div>
     </div>
