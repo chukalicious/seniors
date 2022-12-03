be50b3e30424199f487fc2986bc070e7ffa1c375
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-02 22:52:17 -0800

    made minor spacing adjustments

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
           California Aging Services are here for you!
         </p>
       </div>
-      <div className="w-[100%] flex flex-col mt-4">
+      <div className="w-[100%] flex flex-col mt-4 px-2">
         <div className="card min-w-fit bg-base-100 shadow-xl my-2 mx-auto">
           <div className="card-body">
             <h2 className="card-title">Card title!</h2>
