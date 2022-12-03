45b8eb829f038bf38b458e152e8ab0dbcce4ba45
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-02 21:17:16 -0800

    created navbar component and imported it to app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import "./App.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
 
+import Navbar from "./common/Navbar";
+import Home from "./components/Home";
+
 function App() {
   useEffect(() => {
     themeChange(false);
     // 👆 false parameter is required for react project
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <Navbar /> <Home />
+    </div>
+  );
 }
 
 export default App;
