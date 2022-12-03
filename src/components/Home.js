1beb042eceb701893587fb596552c72272fd9c1d
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-03 01:54:26 -0800

    adjusted the hero component for large screens and added a bouncing icon in the large screen to indicate to the user they have to scroll down to see the rest of the page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
+import { FaChevronDown } from "react-icons/fa";
+
 const Home = () => {
   return (
     <div
-      className="hero min-h-[65vh]"
+      className="hero min-h-[65vh] lg:min-h-screen"
       style={{
         backgroundImage: `url("https://live.staticflickr.com/65535/52539291794_382460c3b1_o.jpg")`,
       }}
@@ -9,9 +11,16 @@ const Home = () => {
       <div className="hero-overlay"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-sm mt-40 md:w-md">
-          <h1 className="mt-44 text-4xl font-bold uppercase">
+          <h1 className="mt-44 text-4xl font-bold uppercase md:mt-40">
             Your one-stop Resource Guide
           </h1>
+          <p className="md:my-5 md:text-xl md:font-semibold">
+            Peruse this guide to find out what resources and services are
+            available to you.
+          </p>
+          <p className="hidden md:flex text-8xl font-semibold w-full mt-48">
+            <FaChevronDown className="text-center mx-auto animate-bounce" />
+          </p>
         </div>
       </div>
     </div>
