a89b941c62ba2e9be9d1982cd51a1d124e15d1ee
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-02 21:52:51 -0800

    deleted redundant bar

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,86 +1,47 @@
 const Navbar = () => {
   return (
-    <>
-      <div className="navbar bg-neutral text-neutral-content">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              What's Inside
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Over 60? California has a plan for you</a>
-              </li>
-              <li>
-                <a>
-                  Services for Older Adults For aging adults in each region of
-                  California
-                </a>
-              </li>
-              <li>
-                <a>Vaccines Provide a Pathway to connection</a>
-              </li>
-              <li>
-                <a>Feeling Isolated? Here's how to reconnect</a>
-              </li>
-              <li>
-                <a>
-                  Elder Abuse and Neglect How to spot it and how you can help
-                </a>
-              </li>
-              <li>
-                <a>Directory of Resources List of older adults resources</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case text-xl">
-            Aging in California
-          </a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
+    <div className="navbar bg-neutral text-neutral-content">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            What's Inside
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
             <li>
-              <a>Item 1</a>
+              <a>Over 60? California has a plan for you</a>
+            </li>
+            <li>
+              <a>
+                Services for Older Adults For aging adults in each region of
+                California
+              </a>
+            </li>
+            <li>
+              <a>Vaccines Provide a Pathway to connection</a>
+            </li>
+            <li>
+              <a>Feeling Isolated? Here's how to reconnect</a>
+            </li>
+            <li>
+              <a>Elder Abuse and Neglect How to spot it and how you can help</a>
+            </li>
+            <li>
+              <a>Directory of Resources List of older adults resources</a>
             </li>
             <li tabIndex={0}>
-              <a>
+              <a className="justify-between">
                 Parent
                 <svg
                   className="fill-current"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
+                  width="24"
+                  height="24"
                   viewBox="0 0 24 24"
                 >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
                 </svg>
               </a>
               <ul className="p-2">
@@ -97,13 +58,41 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        <a className="btn btn-ghost normal-case text-xl">Aging in California</a>
       </div>
-      <div className="navbar bg-primary text-primary-content flex max-h-[5vh] ">
-        <p className="text-center uppercase text-base w-full px-4 font-semibold py-2">
-          Your one-stop Resource Guide
-        </p>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Parent
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 };
 
