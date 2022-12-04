70b6d4898b3bb590307da47c96990868633d32a6
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-04 00:18:57 -0800

    adjusted spacing for the text

--- src/components/Over60.js
+++ src/components/Over60.js
@@ -1,79 +1,135 @@
 const Over60 = () => {
   return (
     <div>
-      <div className="max-w-md py-4 px-4">
-        <p className="p-2 text-justify">
-          State officials predict California will be home to some 10.8 million
-          people aged 60-plus by 2030-nearly a quarter of the state's
-          population. This demographic shift will require dramatically increased
-          access to affordable housing, health care, and social services.{" "}
-        </p>
-        <p className="p-2 text-justify">
-          Recognizing that this demographic change is sure to impact almost
-          every aspect of life, governor Gavin Newsom issued an executive order
-          in 2019 calling for the creation of a plan.
-        </p>
-        <p className="p-2 text-justify">
-          Relese last year, California's{" "}
-          <span className="capitalize">master plan for Aging</span> affirms the
-          need for policies that protect the older population and prepare the
-          state for a "huge demographic change" according to the California
-          Department of Aging (CDA) Director Susan DeMarois.
-        </p>
-        <p className="p-2 text-justify">
-          The <span className="capitalize">master plan for Aging</span>{" "}
-          establishes 5 primary goals, including housing for all ages and
-          stages, health reimagined, equity and inclusion, caregiving that
-          works, and affordable aging.
-        </p>
-        <p className="p-2 text-justify">
-          The plan acknowledges that "affordable aging" is becoming more
-          difficult to find. Almost 30% of older Californians currently live in
-          poverty, and more than 2 million older adults in California are
-          economically insecure.
-        </p>
-        <p className="p-2 text-justify">
-          California's plan proposes more affordable housing units with
-          "disability friendly" transportation networks, outdoors and community
-          spaces for all ages, and improved emergency responses and
-          preparedness.{" "}
-        </p>
-        <p className="p-2 text-justify">
-          While efforts to create the plan began prior to the pandemic, the
-          onset of COVID-19 emphasized the urgency of developing a plan to
-          address aging. In his letter on the Master Plan for Aging, Newsom said
-          the outbreak “exposed persistent and systemic inequities” in the
-          state, and strained aging and disability services “like never before”.
-        </p>
-        <p className="p-2 text-justify">
-          With this in mind, the governor's plan aims to increase the number of
-          caregiver jobs by expanding support for these workers through paid
-          family leave, multilingual training, and virtual care options.{" "}
-        </p>
-        <p className="p-2 text-justify">
-          About $3 billion has been invested in the master plan and its
-          implementation so far, but the total cost of implementation has not
-          yet been determined.
-        </p>
-        <p className="p-2 text-justify">
-          According to DeMarois, prior plans to address aging focused narrowly
-          on heath care. This version, however, seeks to address the full array
-          of challenges and opportunities faced by older adults across the
-          state.
-        </p>
-        <p className="p-2 text-justify">
-          "This master plan is very comprehesive", she says, "it looks at the
-          whole person."{" "}
-        </p>
-        <p className="p-2 text-justify">
-          Perhaps more importantly, the plan was a universally supported
-          endeavor by the Governor, State Legislature and stakeholders, she
-          said.
-        </p>
-        <p className="p-2 text-justify">
-          If you're a California resident age 60 and above, or a caregiver to
-          someone who is, the Master Plan for Agi/ng is for you.
-        </p>
+      <div className="max-w-md py-4 px-4 text-2xl">
+        <div>
+          <h2 className="p-2 font-bold uppercase text-neutral text-3xl">
+            Over 60?
+          </h2>
+          <h3 className="px-2 mt-[-.5rem] pb-4 font-semibold capitalize text-primary text-xl">
+            California has a plan for you
+          </h3>
+          <p className="p-2 text-justify">
+            State officials predict California will be home to some 10.8 million
+            people aged 60-plus by 2030-nearly a quarter of the state's
+            population. This demographic shift will require dramatically
+            increased access to affordable housing, health care, and social
+            services.{" "}
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            Recognizing that this demographic change is sure to impact almost
+            every aspect of life, governor Gavin Newsom issued an executive
+            order in 2019 calling for the creation of a plan.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            Relese last year, California's{" "}
+            <span className="capitalize">master plan for Aging</span> affirms
+            the need for policies that protect the older population and prepare
+            the state for a "huge demographic change" according to the
+            California Department of Aging (CDA) Director Susan DeMarois.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            The <span className="capitalize">master plan for Aging</span>{" "}
+            establishes 5 primary goals, including housing for all ages and
+            stages, health reimagined, equity and inclusion, caregiving that
+            works, and affordable aging.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            The plan acknowledges that "affordable aging" is becoming more
+            difficult to find. Almost 30% of older Californians currently live
+            in poverty, and more than 2 million older adults in California are
+            economically insecure.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            California's plan proposes more affordable housing units with
+            "disability friendly" transportation networks, outdoors and
+            community spaces for all ages, and improved emergency responses and
+            preparedness.{" "}
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            While efforts to create the plan began prior to the pandemic, the
+            onset of COVID-19 emphasized the urgency of developing a plan to
+            address aging. In his letter on the Master Plan for Aging, Newsom
+            said the outbreak “exposed persistent and systemic inequities” in
+            the state, and strained aging and disability services “like never
+            before”.
+          </p>
+        </div>
+        <div>
+          <p className="p-3 text-bold text-primary text-2xl text-center">
+            At 81.9 years, California has the second highest average life
+            expectancy in the nation
+          </p>
+        </div>
+        <div className="p-2">
+          <figure>
+            <img
+              src="https://live.staticflickr.com/65535/52541499229_3cbcf25bfa_z.jpg"
+              alt="map of california with county lines"
+            />
+          </figure>
+          <div className="z-10 bg-secondary mask mask-circle min-w-sm my-4 min-h-fit mt-[-25rem] mb-28 ml-20">
+            <p className="text-8xl text-center p-10 text-base-100  font-semibold">
+              81.9{" "}
+              <span className="uppercase text-6xl mt-[-1.5rem] font-medium">
+                years
+              </span>
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            With this in mind, the governor's plan aims to increase the number
+            of caregiver jobs by expanding support for these workers through
+            paid family leave, multilingual training, and virtual care options.{" "}
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            About $3 billion has been invested in the master plan and its
+            implementation so far, but the total cost of implementation has not
+            yet been determined.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            According to DeMarois, prior plans to address aging focused narrowly
+            on heath care. This version, however, seeks to address the full
+            array of challenges and opportunities faced by older adults across
+            the state.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            "This master plan is very comprehesive", she says, "it looks at the
+            whole person."{" "}
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            Perhaps more importantly, the plan was a universally supported
+            endeavor by the Governor, State Legislature and stakeholders, she
+            said.
+          </p>
+        </div>
+        <div>
+          <p className="p-2 text-justify">
+            If you're a California resident age 60 and above, or a caregiver to
+            someone who is, the Master Plan for Aging is for you.
+          </p>
+        </div>
       </div>
     </div>
   );
