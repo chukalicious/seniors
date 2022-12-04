77cb828b1c22682000b258afb49f137b487a836a
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-04 01:20:39 -0800

    rounded link borders more and linked to the over 60 component

--- src/components/Inside.js
+++ src/components/Inside.js
@@ -6,12 +6,14 @@ const Inside = () => {
         <h2 className="btn btn-ghost text-2xl uppercase">What's Inside</h2>
       </div>
       <div className="flex flex-col text-primary px-2 py-4 md:grid grid-cols-2 gap-4">
-        <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
-          <h3 className="text-2xl text-bold uppercase">Over 60?</h3>
-          <p className="capitalize">California has a plan for you</p>
-        </div>
+        <Link to="/over60">
+          <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
+            <h3 className="text-2xl text-bold uppercase">Over 60?</h3>
+            <p className="capitalize">California has a plan for you</p>
+          </div>
+        </Link>
         <Link to="/services">
-          <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
+          <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
             <h3 className="text-2xl text-bold uppercase">
               Services for older adults
             </h3>
@@ -19,24 +21,24 @@ const Inside = () => {
           </div>
         </Link>
         <Link to="/vaccines">
-          <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
+          <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
             <h3 className="text-2xl text-bold uppercase">
               Vaccines provide a pathway
             </h3>
             <p>to connection</p>
           </div>
         </Link>
-        <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
+        <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
           <h3 className="text-2xl text-bold uppercase">Feeling Isolated?</h3>
           <p>Here's how to reconnect</p>
         </div>
-        <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
+        <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
           <h3 className="text-2xl text-bold uppercase">
             Elder abuse and Neglect
           </h3>
           <p>How to spot it and how you can help</p>
         </div>
-        <div className="hover:bg-primary hover:text-neutral rounded-sm p-2 my-2">
+        <div className="hover:bg-primary hover:text-neutral rounded-lg p-2 my-2">
           <h3 className="text-2xl text-bold uppercase">
             Directory of resources
           </h3>
