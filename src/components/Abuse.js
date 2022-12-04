510eca7270e082921335c697151e717f169d4e56
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-04 15:32:56 -0800

    created and added text to the abuse and reconnect pages

---
+++ src/components/Abuse.js
@@ -0,0 +1,163 @@
+const Abuse = () => {
+  return (
+    <div>
+      <div>Elder abuse and neglect</div>
+      <div>
+        expo text
+        <div>
+          <p>
+            Elder abuse is on the rise in California. More than 200,000 older
+            and dependent abuse cases are reported in California each year, and
+            it is believed that abuse of older adults is significantly
+            underreported.
+          </p>
+        </div>
+        <div>
+          <p>
+            Blanca Castro, the state's Long-Term Care Ombudsman, shared ways to
+            spot elder abuse and how you can advocate for loved ones. Types of
+            abuse include self-neglect, neglect by others, physical abuse,
+            mental abuse, sexual abuse, financial abuse, and abandonment.
+          </p>
+        </div>
+        <div>
+          <p>
+            Castro explained how to determine whether your loved one might be a
+            victim of abuse. "Use all five senses," she said. "Look around their
+            room, smell them. Are they acting normal? Is any behavior out of the
+            norm? There are signs that something could be happening."
+          </p>
+        </div>
+        <div>
+          <p>
+            If something seems off, Castro urges family members to ask for a
+            doctor's assessment. "It is crucial to make sure that they -those in
+            long-term care facilities- are okay and taken care of, and know that
+            you have a right to ensure that they are safe."
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Protecting residents: Know your rights</h2>
+        <p>
+          All residents in long-term facilities across the state are entitled to
+          rights and protections. Residents and their families need to know what
+          these are in order to advocate for themselved and their loved ones.{" "}
+        </p>
+        <h3>Know your rights</h3>
+        <ul>
+          <li>the right to know changes of care</li>
+          <li>the right to spend timein the way you want</li>
+          <li>the right to visitors</li>
+        </ul>
+      </div>
+      <div>
+        <div>
+          <h2>how to stop it and how you can help</h2>
+        </div>
+        <div>
+          <p>
+            The California Department of Public Helth lists the rights of
+            long-term care residents and the rights of their families on their
+            website (www.cdph.ca.gov). For residents, these rights include the
+            right to know of changes to their care, the right to spend time in
+            the way that they want to, amd the right to visitors.
+          </p>
+        </div>
+        <div>
+          <h4>How to advocate</h4>
+          <p>
+            Through ombudsman, family members and residents can advocate for
+            their rights and ensure that they are traeted fairly. Family members
+            have the right to speak up for their loved ones if they cannot
+            communicate for themselves.
+          </p>
+          <p>
+            If you or a loved one has been a victim of elder abuse, there are
+            excellent resources and opportunities available through the
+            Department of Social Services (www.cdss.ca.gov), the Department of
+            Aging (www.aging.ca.gov), to help you seek justice.
+          </p>
+        </div>
+        <div>
+          <h4>To protect older adults from abuse and neglect:</h4>
+          <p>
+            Call 1-800-231-4024, for individuals living in long-tem care
+            facilities, to reach the Long Tem Care Ombudsman CRISIS Line.
+          </p>
+          <p>
+            Call 1-833-401-0832, for individuals living at home, to talk to
+            Adult Protective Services.
+          </p>
+        </div>
+        <div>
+          <h4>Volunteer opportunities</h4>
+          <p>
+            Local Long-Term Ombudsman programs are always recruiting vounteers.
+            If you want to get involcved in the ombudsman program or know
+            someone who does, contact us at stateomb@aging.ca.gov.
+          </p>
+          <p></p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3 className="capitalize">7 sings of abuse</h3>
+          <h4>The most common are sexual, emotional & physical abuse</h4>
+        </div>
+        <div>
+          <ul>
+            <li>self-neglect</li>
+            <li>neglect by others</li>
+            <li>physical abuse</li>
+            <li>sexual abuse</li>
+            <li>mental abuse</li>
+            <li>financial abuse</li>
+            <li>abandonment</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">What does fraud look like?</h2>
+            <h3>top 5 scam categories</h3>
+          </div>
+          <div className="card-body">
+            <ul>
+              <li>
+                25% <span>Identity Theft</span>
+              </li>
+              <li>
+                16% <span>Imposter Scams</span>
+              </li>
+              <li>
+                11%{" "}
+                <span>
+                  Credit Bureaus, Information Furnishers & Report Users
+                </span>
+              </li>
+              <li>
+                7% <span>Online Shopping & Negative Reviews</span>
+              </li>
+              <li>
+                4%s <span>Bank & Lenders</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <h3>Signs of elder financial abuse</h3>
+          <ul>
+            <li>unusual financial activity</li>
+            <li>new "friends" or helpers</li>
+            <li>mobility or frailty issues</li>
+            <li>cognitive decline or loss of financial acumen</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Abuse;
